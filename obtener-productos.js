0a1da14aca80b4d6729ac4547ff08201eb84f21f
KNCprograma <110071426+KNC96@users.noreply.github.com>
2023-10-21 13:50:05 -0300

    agregue id
    agregue contador para que me genere un id

--- obtener-productos.js
+++ obtener-productos.js
@@ -2,6 +2,7 @@ const cheerio = require("cheerio");
 const request = require("request");
 const express = require("express");
 const router = express.Router();
+let contador = 0;
 
 const url =
   "https://listado.mercadolibre.com.ar/hogar-muebles-jardin/iluminacion-hogar/lamparas-en-cordoba/lampara-madera_CostoEnvio_Gratis_NoIndex_True#applied_filter_id%3Dshipping_cost_highlighted_free%26applied_filter_name%3DCosto+de+env%C3%ADo%26applied_filter_order%3D3%26applied_value_id%3Dfree%26applied_value_name%3DGratis%26applied_value_order%3D1%26applied_value_results%3D271%26is_custom%3Dfalse";
@@ -19,9 +20,11 @@ let obtenerProductos = () => {
           let imagen = $(this)
             .find(".ui-search-result__image img")
             .attr("data-src");
+          contador++;
 
           // Crear un objeto para el elemento actual
           let item = {
+            id: contador,
             titulo: titulo,
             precio: precio,
             imagen: imagen,
